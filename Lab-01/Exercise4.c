874bfb0f5db5ba55931ea81b39cc6dda55eaaaef
Nguyen Trung Ky <nguyen.ky211@gmail.com>
2022-03-27 12:49:45 +0300

    Initial commit

---
+++ Lab-01/Exercise4.c
@@ -0,0 +1,19 @@
+/*
+4.	Write a program to display a hollow triangle by asterisks (*) on the screen.
+The input will be the height of the triangle and the thickness of the borders.
+Ex:
+ ____________________________________
+| Input: 7 2                         |
+| Output:                            |
+|             *                      |
+|           * * *                    |
+|         * *   * *                  |
+|       * *       * *                |
+|     * *           * *              |
+|   * * * * * * * * * * *            |
+| * * * * * * * * * * * * *          |
+|____________________________________|
+*/
+
+//TODO
+//WRITE YOUR CODE HERE
